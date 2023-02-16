12daf6fb7a622755a8267ed811bdde192b276dbc
PRATAP-KUMAR <pratap@fastmail.fm>
2023-02-17 00:18:56 +0530

    Base functionality of button working

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
 import './App.css';
 
 function App() {
+  const dom = () => {
+    /* eslint-disable no-undef */
+    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+      const activeTabId = tabs[0].id
+      chrome.scripting.executeScript({
+        target: { tabId: activeTabId },
+        function: () => alert("HI"),
+      });
+    })
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
+    <div className="extension-container">
+      <h2>
         My first chrome extension creation
-      </header>
+      </h2>
+      <button
+        type='button'
+        aria-label='click to auto click on connect button'
+        onClick={dom}
+      >
+        Start Connecting
+      </button>
     </div>
   );
 }

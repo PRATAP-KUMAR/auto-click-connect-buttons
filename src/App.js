a04a6aaf9374d0c616e2f4193a81da52149ce903
PRATAP-KUMAR <pratap@fastmail.fm>
2023-02-17 11:55:57 +0530

    useState, useEffect are taking too long to get data from function opposed to component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,51 @@
-import Logic from './Logic/Logic';
+import { useState } from 'react';
 import './App.css';
+import { CountdownCircleTimer } from 'react-countdown-circle-timer';
 
 function App() {
+
+  const [isActive, setActive] = useState(false);
+
+  const Script = () => {
+    const buttons = document.querySelectorAll('[aria-label^="Invite"]');
+    buttons.forEach((btn, idx) => {
+      setTimeout(() => {
+        btn.remove();
+      }, 5000 * (idx + 1));
+    })
+  }
+
   const dom = () => {
     /* eslint-disable no-undef */
+    setActive(true);
     chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
       const activeTabId = tabs[0].id
       chrome.scripting.executeScript({
         target: { tabId: activeTabId },
-        function: Logic,
-      });
+        function: Script
+      })
     })
   }
 
   return (
-    <div className="extension-container">
+    <div className="extension-container" id='CONNECT-EXTENSION'>
       <h2>
-        My first chrome extension creation
+        Send Inivations
       </h2>
+      {
+        isActive ?
+          <CountdownCircleTimer
+            isPlaying
+            duration={20}
+            colors={['#004777', '#F7B801', '#A30000', '#A30000']}
+            colorsTime={[7, 5, 2, 0]}
+          >
+            {({ remainingTime }) => remainingTime}
+          </CountdownCircleTimer> :
+          null
+      }
       <button
         type='button'
-        id='CONNECT-EXTENSION'
         aria-label='click to auto click on connect button'
         onClick={dom}
       >
@@ -29,5 +54,7 @@ function App() {
     </div>
   );
 }
+
+
 
 export default App;

08198cc2cbb2382b7a2ee7081c5a25eb9d1bca34
PRATAP-KUMAR <pratap@fastmail.fm>
2023-02-18 14:44:20 +0530

    State managed with chrome.storage.local

--- src/App.js
+++ src/App.js
@@ -1,60 +1,102 @@
-import { useState } from 'react';
+/* eslint-disable no-undef */
+import { useEffect, useState } from 'react';
 import './App.css';
 import { CountdownCircleTimer } from 'react-countdown-circle-timer';
 
 function App() {
-
   const [isActive, setActive] = useState(false);
+  const [timer, setTimer] = useState(20);
+  const [count, setCount] = useState(0);
 
-  const Script = () => {
+  useEffect(() => {
+    if (timer > 0) {
+      chrome.storage.local.get(["timer"]).then((result) => {
+        setTimer(result.timer);
+      })
+    }
+  }, [timer])
+
+  useEffect(() => {
+    if (count === 0 || count > 0) {
+      chrome.storage.local.get(["count"]).then((result) => {
+        setCount(result.count);
+      })
+    }
+  }, [count])
+
+  function script() {
     const buttons = document.querySelectorAll('[aria-label^="Invite"]');
+    chrome.storage.local.set({ timer: buttons.length })
+
     buttons.forEach((btn, idx) => {
       setTimeout(() => {
-        btn.remove();
+        btn.scrollIntoView({ behavior: "smooth", block: "center", inline: "center" });
+        btn.style.backgroundColor = "red";
+        chrome.storage.local.set({ count: idx + 1 })
       }, 5000 * (idx + 1));
     })
   }
 
-  const dom = () => {
-    /* eslint-disable no-undef */
+  const dom = (e) => {
+    e.target.remove();
     setActive(true);
+    chrome.storage.local.set({ count: 0 })
     chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
       const activeTabId = tabs[0].id
       chrome.scripting.executeScript({
         target: { tabId: activeTabId },
-        function: Script
+        function: script
       })
     })
   }
 
+  // const children = ({ remainingTime }) => {
+  //   return (remainingTime/5)
+  // }
+
+  const children = ({ remainingTime }) => {
+    const value = Math.abs(remainingTime / 5 - 5);
+    if (remainingTime % 5 === 0)
+      return value
+    return Math.floor(value);
+  }
+
   return (
     <div className="extension-container" id='CONNECT-EXTENSION'>
-      <h2>
-        Send Inivations
+      <h2 className='header-bar'>
+        LinkedIn AutoConnect
       </h2>
       {
         isActive ?
-          <CountdownCircleTimer
-            isPlaying
-            duration={20}
-            colors={['#004777', '#F7B801', '#A30000', '#A30000']}
-            colorsTime={[7, 5, 2, 0]}
-          >
-            {({ remainingTime }) => remainingTime}
-          </CountdownCircleTimer> :
-          null
+          <h3 className='invitations-count'>
+            Invitations Sent
+          </h3> :
+          <h3>
+            &darr;
+          </h3>
       }
+      <div className='status'>
+        {
+          isActive ?
+            <CountdownCircleTimer
+              isPlaying
+              duration={timer * 5}
+              colors={['#4dba12']}
+            >
+              {children}
+            </CountdownCircleTimer> :
+            null
+        }
+      </div>
       <button
         type='button'
         aria-label='click to auto click on connect button'
         onClick={dom}
       >
-        Start Connecting
+        {isActive ? "Stop Connecting" : "Start Connecting"}
       </button>
     </div>
   );
 }
-
-
 
 export default App;
